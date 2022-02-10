b79b2f7a56dc650d9e56f40f699c60700d9825b6
Mamba2410 <callummcgregor54@gmail.com>
2022-02-10 13:09:05 +0000

    docs: Cataloguer

--- pipeline/Cataloguer.py
+++ pipeline/Cataloguer.py
@@ -12,47 +12,60 @@ import Constants
 
 
 class Cataloguer:
-    """
-    Cataloguer class
-
-
-    """
 
     config = None           # Config object
-    n_sources = 0           # Number of sources
-    wcs = None          ## TODO: Do something with this
+    n_sources = 0           # Number of sources discovered
+    wcs = None              ## TODO: Do we need to keep this?
 
     
     def __init__(self, config):
         """
+        Cataloguer class
+
+        Does all the handling of identifying and cataloguing stars
+        in a field.
+        
+        Uses DAO star finder to get ids and basic info.
+        Uses astrometry.net local solving to find coordinate system.
+
         Parameters
         ----------
 
         config: Constants.Config
+            Config object used for the run
         """
+
         self.config = config
 
 
     def generate_catalogue(self, image_path, solve=True):
         """
-        Generates a catalogue of all the stars in the given image, as well as
-        a list of the times at which each image was taken.
+        Generates a catalogue of all the stars in the given image.
+        Catalogue location is defined in self.config.catalogue_prefix.
 
         Parameters
         ----------
-        image_path: string
-            path to the fits image to catalogue all the stars.
-        solve: bool, optional
-            should solve astrometry.net wcs?
 
+        image_path: string
+            Absolute path to the fits image to catalogue all the stars.
+
+        solve: bool, optional
+            Should we solve astrometry.net wcs?
+
+        Returns
+        -------
+
+        n_sources: int
+            How many sources we found in the image.
 
         """
 
-        #read in image data
+        ## Read in image data
         image_data = fits.getdata(image_path, ext=0)
         
         ## TODO: Magic number, threshold passed to find_stars
-        #build a catalogue of all stars in the image
+        ## Build a catalogue of all stars in the image
+        ## sources is a Table object
         sources = find_stars(image_data, 5)
         self.filter_stars(sources, image_data)
         self.n_sources = len(sources['id'])
@@ -63,10 +76,10 @@ class Cataloguer:
         ## TODO: Necessary?
         #Utilities.make_reg_file(self.config.workspace_dir, self.config.image_prefix, sources)
         
-        #add the RA and DEC for each star to the catalogue
+        ## Add the RA and DEC for each star to the catalogue
         self.convert_to_ra_and_dec(image_path, sources, solve=solve)
         
-        #write the catalogue to the catalogue file
+        ## Write the catalogue to the catalogue file
         sources.write(self.config.catalogue_path, format=self.config.table_format, overwrite=True)
 
         return self.n_sources
@@ -77,18 +90,29 @@ class Cataloguer:
     def filter_stars(self, sources, image_data):
         """
         Removes stars from a source catalogue given cutoff parameters
-        from the config object
+        defined in the config object.
+
+        Note: 'id' column in sources is now no longer a continuous
+        range from 0..n_sources
+
+        Parameters
+        ----------
 
         sources: Table
+            Information on all of the sources found.
             
-        image_data: 
+        image_data: fits data
+            Data of the fits image used to create the catalogue.
             
         """
         
         to_remove = []
                 
+        ## Loop over each source
         for i in range(self.n_sources):
             
+            ## Conditions to remove a source
+            ## Note: is_within_boundaries does not account for later shifts
             is_too_bright = sources['flux'][i] > self.config.flux_cutoff
             is_within_boundaries = Utilities.is_within_boundaries(
                     sources['xcentroid'][i],
@@ -117,23 +141,38 @@ class Cataloguer:
         """
         Converts all the source positions in the image to RA and DEC.
         Uses World Coordinate System (wcs) from the fits image.
+        Does this via astrometry.net api.
+
+        Sets the self.wcs variable to the WCS header found.
+
+        Parameters
+        ----------
+
+        image_file: string
+            Absolute path to the image to use as catalogue.
+
+        sources: Table
+            Table of information on all the sources.
+
+        solve: bool, optional
+            Should we solve the header using astrometry.net?
 
         """
         
         print("[Cataloguer] Getting coordinate system for image '{}'".format(image_file))
 
-        # find the wcs assosiated with the fits image using astropy and the header
+        ## Find the wcs assosiated with the fits image using astropy and the header
         self.wcs = self.get_wcs_header(image_file, solve=solve)
         
-        ## make two new coloums of 0's
+        ## Make two new coloums in source table, set to zero
         #sources['RA'] = sources['xcentroid'] * 0
         #sources['DEC'] = sources['xcentroid'] * 0
     
-        ## replace the 0's with ra and dec
-        #for x in range(0,len(sources)):
-        #    ra, dec = self.wcs.all_pix2world(sources['xcentroid'][x], sources['ycentroid'][x], 0) 
-        #    sources['RA'][x] = ra
-        #    sources['DEC'][x] = dec
+        ## Replace the zeroes with ra and dec
+        #for i in range(self.n_sources):
+        #    ra, dec = self.wcs.all_pix2world(sources['xcentroid'][i], sources['ycentroid'][i], 0) 
+        #    sources['RA'][i] = ra
+        #    sources['DEC'][i] = dec
     
 
 
@@ -147,33 +186,46 @@ class Cataloguer:
 
         Parameters
         ----------
+
         file: string
-            File to send to astrometry.net
+            Absolute path to file to solve with astrometry.net
+
+        solve: bool, optional
+            Should we do the solve? (used for debug as the process can be slow)
 
         Returns
         -------
-        WCS
-            FITS world coordinate system
+
+        wcs: WCS
+            FITS world coordinate system header
+
         """
 
+        ## Set up astrometry.net instance
         ast = AstrometryNet()
         ast.TIMEOUT = self.config.astrometry_timeout
         ast.api_key = self.config.api_key
     
         wcs = None
+
+        ## If we don't want to solve, return empty header
         if not solve:
             return WCS(header=wcs)
     
+
         print("[Astrometry] Starting job")
         try:
-            ## Solve on local machine
+            ## Try to solve on local machine
             wcs = ast.solve_from_image(
                     file, 
                     solve_timeout=self.config.astrometry_timeout,
                     force_image_upload=False)
         except Exception as e:
-            print("\n[Astrometry] Error: WCS failed")
+            ## If we failed, print why (likely timeout)
+            print("\n[Astrometry] Error: WCS solve failed")
             print("............ Exception: {}".format(e))
+            print("............ (self.config.astrometry_timeout={}s)"
+                    .format(self.config.astrometry_timeout))
         
         print("\n[Astrometry] Finished job")
             
@@ -185,7 +237,8 @@ class Cataloguer:
 
     def generate_image_times(self):
         """
-        Get a list of times of each measurement
+        Get a list of times of each image/measurement.
+        File path specified in self.config.
 
         """
 
@@ -197,13 +250,15 @@ class Cataloguer:
         ## Open time file
         with open(self.config.time_path, "a+") as time_file:
     
-            #loop through all images within each set 
+            ## Loop through all images
             for image_name, _s, _i in Utilities.loop_images(self.config):
             
+                ## Get the image header
                 image_file = os.path.join(self.config.image_dir, image_name)
                 image_header = fits.getheader(image_file)
 
                 ## Write time to file
+                ## TODO: Sometimes get warning on name
                 time_file.write("{}{}".format(
                             image_header['DATE-OBS'],
                             self.config.line_ending))
@@ -216,30 +271,39 @@ class Cataloguer:
 def find_stars(image_data, threshold):
     """
     Catalogue all sources that meet the thresholds in the image.
+    Uses DAOStarFinder.
 
     Parameters
     ----------
+
     image_data: fits image data
         Image data to find stars in.
 
     threshold: float
-        Number of standard deviations above which is a star.
+        Number of standard deviations above the background which
+        an object is classed as a star.
+
+    Returns
+    -------
+
+    sources: Table
+        Table of all the information DAO found about the star.
 
     """
     
-    #get mean median and standard deviation of the image data
+    ## Get mean median and standard deviation of the image data
     mean, median, std = sigma_clipped_stats(image_data, sigma=3.0, maxiters=5)  
     
-    #initiate finder object. Will find objects with a FWHM of 8 pixels
-    # and 3-sigma times the background
+    ## Initiate finder object. Will find objects with a FWHM of 8 pixels
+    ## and 3-sigma times the background
     daofind = DAOStarFinder(fwhm=8, threshold=threshold*std) 
     
-    #finds sources
+    ## Find sources
     sources = daofind(image_data)
 
-        
+    ## Set table format
     for col in sources.colnames:    
-        sources[col].info.format = '%.8g'  # for consistent table output
+        sources[col].info.format = '%.8g'
        
     return sources
 
