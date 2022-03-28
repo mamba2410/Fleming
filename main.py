ffb617cae1cdbc3418d5791009c3f072434a00e5
Mamba2410 <callummcgregor54@gmail.com>
2022-03-28 16:28:06 +0100

    main: update hand picked

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy.coordinates import SkyCoord 
 
 def main():
     """
@@ -64,8 +66,8 @@ def main():
             #[raw_base_dir+"/2019/0226", "l196",         7,      "noflat",     "bias_end"],
             #[raw_base_dir+"/2019/0225", "l196_5",       6,      "noflat",     "bias_end"],
             #[raw_base_dir+"/2019/0204", "l197",         7,      "noflat",     "Bias"],
-            [raw_base_dir+"/2019/0131", "l197.5",       9,      "noflat",     "biasend"],
-            #[raw_base_dir+"/2019/0128", "l198",         7,      "noflat",     "bias_2"],
+            #[raw_base_dir+"/2019/0131", "l197.5",       9,      "noflat",     "biasend"],
+            [raw_base_dir+"/2019/0128", "l198",         7,      "noflat",     "bias_2"],
             #[raw_base_dir+"/2019/0129", "l198_5",       7,      "noflat",     "bias-1"],
             #[raw_base_dir+"/2019/0207", "l199",         7,      "noflat",     "Bias"],
     ]
@@ -89,8 +91,8 @@ def main():
             catalogue_image_number = 1,
         )
 
-        Pipeline.run(config)
-        #Pipeline.run_analysis(config, assume_already_adjusted=False)
+        #Pipeline.run(config)
+        Pipeline.run_analysis(config, assume_already_adjusted=True)
 
 
     ## ===================================================
@@ -129,33 +131,92 @@ def hand_pick():
     config = Config()
 
     hand_picked = [
-        ["l135",   1005, 1],
-        ["l136_5",  599, 2],
-        ["l138_0", 1194, 3],
-        ["l138_0", 1384, 2], ## Eclipse?
-        ["l140_0",  760, 1],
-        ["l141_5",  276, 2],
-        ["l141_5",  448, 1],
-        ["l196",    507, 4], ## Sharp bottom, not sine?
-        ["l196",   1287, 2], ## Eclipse
-        ["l196",   1637, 2], ## Ditch?
-        ["l197.5", 1235, 0], ## Doesn't pick up period
-        ["l197",     98, 4], ## Appears irregular
-        ["l197",    164, 3], ## Eclipse?
-        ["l197",    371, 3], ## Maybe irregular?
-        ["l197",    456, 2],
-        ["l197",   1844, 2],
-        ["l198",    482, 1],
-        ["l198",   1577, 1],
+        ["l135",   1005, 1], ## Sine
+        ["l135_5",  148, 0], ## Transit
+        ["l135_5",  590, 0], ## Long period?
+        ["l135_5",  630, 0], ## Transit
+        ["l136_5",  576, 1], ## Transit or sine?
+        ["l136_5",  580, 1], ## Sine
+        ["l136_5",  599, 2], ## Multi-period
+        ["l137_0", 1084, 1], ## Sine
+        ["l138_0", 1194, 3], ## Multi-period
+        ["l138_0", 1384, 0], ## Eclipse?
+        ["l140_0",  760, 1], ## Sine
+        #["l140_0",  762, 1], ## Shadow of above?
+        ["l140_5",  479, 0], ## Sharp transit
+        ["l141",    125, 1], ## Sharp bottom
+        ["l141_5",  276, 2], ## Multi-period
+        ["l141_5",  301, 2], ## Multi-period
+        ["l141_5",  448, 1], ## Sine
+        ["l196",    507, 4], ## Sine, sharp bottom
+        ["l196",   1287, 0], ## Eclipse
+        ["l196",   1416, 0], ## Weird transit
+        ["l196",   1637, 2], ## Transit
+        #["l197.5", 1235, 0], ## Doesn't pick up period
+        ["l197",     98, 4], ## Multi-period, appears irregular
+        ["l197",    164, 0], ## Transit
+        ["l197",    371, 3], ## Multi-period
+        ["l197",    456, 2], ## Multi-period
+        ["l197",    869, 1], ## Sine
+        ["l197",   1077, 1], ## Sine
+        ["l197",   1499, 2], ## Multi-period ?
+        ["l197",   1844, 0], ## Transit
+        ["l197",   2118, 2], ## Irregular?
+        ["l197",   2374, 1], ## Sine, long period
+        ["l198",    482, 1], ## Sine
+        ["l198",   1577, 1], ## Sine
+        #["l198",   1593, 1], ## Sine, shadow
+        ["l198",   2133, 1], ## Flare? Irregular?
+        ["l198",   2268, 0], ## Scoop?
+        ["l198",   2640, 0], ## Transit
     ]
+
+    hand_picked_unsure = [
+        ["l135",    121, 1], # long?
+        ["l135",   1080, 1],
+        ["l135_5",  392, 1],
+        ["l135_5",  398, 1],
+        ["l135_5",  410, 1],
+        ["l135_5",  590, 1], ## 60% 
+        ["l135_5",  637, 1],
+        ["l135_5",  839, 1],
+        ["l135_5",  904, 0],
+        ["l135_5",  955, 1],
+        ["l135_5", 1000, 1],
+        ["l135_5", 1004, 1],
+        ["l137_0",  512, 1],
+        ["l137_0",  686, 1],
+        ["l196",   62, 1],
+        ["l196",   96, 1],
+        ["l196", 1080, 1],
+        ["l196", 1497, 1],
+        ["l196", 1635, 1], # weird transit?
+        ["l197",  444, 1],
+        ["l197",  479, 1], # weird transit?
+        ["l197",  841, 1],
+        ["l197",  957, 1],
+        ["l197", 1118, 1],
+        ["l197", 1293, 1],
+        ["l197", 2140, 0], # transit?
+        ["l197", 2462, 1],
+        ["l198_5", 1340, 1], # weird
+        ["l198_5", 1411, 1], # weird
+        ["l198",  745, 1],
+        ["l198", 1115, 0], # double transit? Shadow?
+        ["l198", 1307, 1], # ??
+        ["l198", 1589, 0], # double transit?
+    ]
+
     n_variables = len(hand_picked)
+    print("[HandPicked] Running for {} variables".format(n_variables))
 
     fname = "results_dr2.txt"
     final_results = open(os.path.join(config.hand_picked_dir, fname), "w")
     final_results.write(
-            "RA [deg]\tDEC [deg]\tperiod [s]\tperiod error [s]\tamplitude [\%]\tamplitude error [\%]\n")
+            "RA [deg]\tDEC [deg]\tField id\tsource id\tperiod [h]\tperiod error [h]\tamplitude [%]\tamplitude error [%]\n")
 
     for field_id, source_id, n_periods in hand_picked:
+    #for field_id, source_id, n_periods in hand_picked_unsure:
         config = Config(
             image_prefix = field_id,
         )
@@ -198,21 +259,24 @@ def hand_pick():
                 lc['counts'] -= fit_sine
 
                 ## Write the period to the file
-                final_results.write("{}\t{}\t{}\t{}\t{}\t{}\n"
-                        .format(ra, dec, P, P_err, A, A_err))
+                final_results.write("{:.6f}\t{:.6f}\t{}\t{:04}\t{:1.5f}\t{:1.5f}\t{:02.4f}\t{:02.4f}\n"
+                        .format(ra, dec, field_id, source_id, P/3600, P_err/3600, 100*A, 100*A_err))
 
-            plt.title("Light curve and fitted plot for variable at RA={:3.6}, DEC={:3.6}"
-                    .format(ra, dec))
+            coord = SkyCoord(ra*u.degree, dec*u.degree)
+            plt.title("Light curve and fitted plot for variable at\n{}"
+                    .format(coord.to_string("hmsdms", precision=0)))
             plt.plot(lc['time']/3600, attempted_fit, color="red")
 
         else:
             ## If we don't have a period, just write coords
-            final_results.write("{}\t{}\t{}\t{}\t{}\t{}\n"
-                    .format(ra, dec, 0, 0, 0, 0))
+            final_results.write("{:.6f}\t{:.6f}\t{}\t{:04}\t{:.0f}\t{:4.0f}\t{:02.4f}\t{:02.4f}\n"
+                    .format(ra, dec, field_id, source_id, 0, 0, 0, 0))
 
-            plt.title("Light curve for variable at RA={:3.6}, DEC={:3.6}"
-                    .format(ra, dec))
+            plt.title("Light curve for variable at\n{}"
+                    .format(coord.to_string("hmsdms", precision=0)))
 
+        plt.xlabel("Time [hours]")
+        plt.ylabel("Normalised flux [arb. u.]")
 
         ## Write the plot to disk
         fname = "final_{}_{}{:04}{}".format(config.image_prefix, config.identifier,
@@ -323,6 +387,6 @@ def rename():
 
 if __name__ == "__main__":
     #rename()
-    main()
-    #hand_pick()
+    #main()
+    hand_pick()
 
